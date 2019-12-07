addab2c76fa4a0ae93263d3660257e0c998c499f
Tim te Beek <tim.te.beek@jdriven.com>
2019-12-07 18:19:58 +0100

    Day 6 part 2

--- src/main/java/com/github/timtebeek/day6/orbits/Orbits.java
+++ src/main/java/com/github/timtebeek/day6/orbits/Orbits.java
@@ -1,19 +1,17 @@
 package com.github.timtebeek.day6.orbits;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toMap;
 
 class Orbits {
 
-	static long totalNumberOfOrbits(List<String> list) {
+	static Map<String, SpaceObject> parseMap(List<String> list) {
 		// Create a map containing all space objects
 		Map<String, SpaceObject> allSpaceObjects = list.stream()
 				.flatMap(line -> Stream.of(line.split("\\)")))
@@ -23,17 +21,45 @@ class Orbits {
 		// Loop over all orbits to assign children
 		list.stream()
 				.map(line -> line.split("\\)"))
-				.forEach(orbit -> allSpaceObjects.get(orbit[0]).getChildren().add(allSpaceObjects.get(orbit[1])));
+				.forEach(orbit -> {
+					SpaceObject left = allSpaceObjects.get(orbit[0]);
+					SpaceObject right = allSpaceObjects.get(orbit[1]);
+					right.setOrbits(left);
+					left.getChildren().add(right);
+				});
+		return allSpaceObjects;
+	}
 
-		SpaceObject centerOfMass = allSpaceObjects.get("COM");
-		System.out.println(centerOfMass);
-		return centerOfMass.countOrbits(0);
+	public static long numberOfTransfers(Map<String, SpaceObject> allSpaceObjects) {
+		SpaceObject youOrbit = allSpaceObjects.get("YOU").getOrbits();
+		SpaceObject sanOrbit = allSpaceObjects.get("SAN").getOrbits();
+
+		List<SpaceObject> yourchain = orbitChain(youOrbit);
+		List<SpaceObject> sanchain = orbitChain(sanOrbit);
+
+		List<SpaceObject> intersections = new ArrayList<>(yourchain);
+		intersections.retainAll(sanchain);
+		SpaceObject first = intersections.get(0);
+
+		return yourchain.indexOf(first) + sanchain.indexOf(first);
+	}
+
+	private static List<SpaceObject> orbitChain(SpaceObject spaceObject) {
+		List<SpaceObject> chain = new ArrayList<>();
+		SpaceObject current = spaceObject;
+		while (current != null) {
+			chain.add(current);
+			current = current.getOrbits();
+		}
+		return chain;
 	}
 }
 
 @Data
+@EqualsAndHashCode(of = "name")
 class SpaceObject {
 	final String name;
+	SpaceObject orbits;
 	Set<SpaceObject> children = new HashSet<>();
 
 	public long countOrbits(int depth) {
--- src/test/java/com/github/timtebeek/day6/orbits/OrbitsTest.java
+++ src/test/java/com/github/timtebeek/day6/orbits/OrbitsTest.java
@@ -3,6 +3,8 @@ package com.github.timtebeek.day6.orbits;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -12,15 +14,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class OrbitsTest {
 
 	@Test
-	void testSample() throws Exception {
+	void testNumberOfOrbitsSample() throws Exception {
 		Path path = Paths.get(getClass().getResource("sample.txt").toURI());
-		assertEquals(42, Orbits.totalNumberOfOrbits(Files.lines(path).collect(Collectors.toList())));
+		List<String> lines = Files.lines(path).collect(Collectors.toList());
+		Map<String, SpaceObject> allSpaceObjects = Orbits.parseMap(lines);
+
+		long totalNumberOfOrbits = allSpaceObjects.get("COM").countOrbits(0);
+		assertEquals(42, totalNumberOfOrbits);
 	}
 
 	@Test
-	void testPart1() throws Exception {
+	void testNumberOfOrbitsPart1() throws Exception {
 		Path path = Paths.get(getClass().getResource("map.txt").toURI());
-		assertEquals(234446, Orbits.totalNumberOfOrbits(Files.lines(path).collect(Collectors.toList())));
+		List<String> lines = Files.lines(path).collect(Collectors.toList());
+		Map<String, SpaceObject> allSpaceObjects = Orbits.parseMap(lines);
+
+		long totalNumberOfOrbits = allSpaceObjects.get("COM").countOrbits(0);
+		assertEquals(234446, totalNumberOfOrbits);
+	}
+
+	@Test
+	void testNumberOfTransfersSample() throws Exception {
+		Path path = Paths.get(getClass().getResource("sample2.txt").toURI());
+		List<String> lines = Files.lines(path).collect(Collectors.toList());
+		Map<String, SpaceObject> allSpaceObjects = Orbits.parseMap(lines);
+
+		assertEquals(4, Orbits.numberOfTransfers(allSpaceObjects));
+	}
+
+	@Test
+	void testNumberOfTransfersPart2() throws Exception {
+		Path path = Paths.get(getClass().getResource("map.txt").toURI());
+		List<String> lines = Files.lines(path).collect(Collectors.toList());
+		Map<String, SpaceObject> allSpaceObjects = Orbits.parseMap(lines);
+
+		assertEquals(385, Orbits.numberOfTransfers(allSpaceObjects));
 	}
 
 }
