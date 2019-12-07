8c712d76d16b10868bd02b1eeb6b6bf932a20770
Tim te Beek <tim.te.beek@jdriven.com>
2019-12-07 09:40:38 +0100

    Rename input files to avoid confusion

--- src/main/java/com/github/timtebeek/day1/fuel/FuelRequirements.java
+++ src/main/java/com/github/timtebeek/day1/fuel/FuelRequirements.java
@@ -4,14 +4,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FuelRequirements {
+class FuelRequirements {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(calculateTotal());
 	}
 
 	static Integer calculateTotal() throws Exception {
-		Path input = Paths.get(FuelRequirements.class.getResource("input.txt").toURI());
+		Path input = Paths.get(FuelRequirements.class.getResource("modules.txt").toURI());
 		return Files.lines(input)
 				.mapToInt(str -> calculateFuel(Integer.parseInt(str)))
 				.sum();
